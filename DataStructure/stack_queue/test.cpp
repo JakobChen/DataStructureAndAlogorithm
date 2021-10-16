09a2fb144fd90af5eb5f8ca821e64930abcb1bd0
JakobChen <jacob.chen@gmx.de>
2021-10-16 18:28:01 +0200

    binary search with unknown size

---
+++ DataStructure/stack_queue/test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+    // 1. Initialize a queue.
+    int* p = nullptr;
+    if(p){
+                // 5. Get the first element.
+        cout << "The first element is: " << endl;
+        // 6. Get the last element.
+        cout << "The last element is: " << endl;
+        // 7. Get the size of the queue.
+        cout << "The size is: " << endl;
+    }
+    return 0;
+
+}
